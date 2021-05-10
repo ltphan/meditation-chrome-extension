e3d7930d1bac348e38dbf55f1d49b14d0c93c396
Linh <linh@linhs-mbp.lan>
2021-05-10 15:31:27 -0700

    removed button for popup and implemented start timer functionality

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const startBtn = document.getElementById('startBtn')
+const timerDisplay = document.getElementById('timerDisplay')
+const pauseBtn = document.getElementById('pauseBtn')
+
+const startingMinutes = parseInt(timerDisplay.innerHTML)
+let time = startingMinutes*60
+
 function createNotification() {    
     chrome.notifications.create('reminder', {
         type: 'basic',
@@ -7,5 +14,28 @@ function createNotification() {
     })
 }
 
+function countDown() {
+    if (time <= 0) {
+        clearInterval(time=0)
+    }
+
+    let minutes = Math.floor(time / 60)
+    let seconds  = Math.floor(time % 60)
+
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    timerDisplay.innerText = `${minutes}:${seconds}`
+
+    time -= 1
+}
+
+function startTimer() {
+    setInterval(countDown,1000)
+}
+
+startBtn.addEventListener('click', startTimer)
+
 const reminderBtn = document.getElementById('reminderBtn')
 reminderBtn.addEventListener('click', createNotification)
+pauseBtn.addEventListener('click', startTimer)
