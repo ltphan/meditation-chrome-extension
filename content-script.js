2c4c198377a46009058324b9535b42829d67fa43
Linh <linh@linhs-mbp.lan>
2021-06-01 12:46:50 -0700

    remove creation of notification in content script

--- content-script.js
+++ content-script.js
@@ -1,10 +1,1 @@
 console.log("content script")
-
-function createNotification() {    
-    chrome.notifications.create('reminder', {
-        type: 'basic',
-        iconUrl: 'waking-up.png',
-        title: title,
-        message: message
-    })
-}
