cbc75ee2d8ee65e2118e72b33753426cd4533947
Linh <linh@linhs-mbp.lan>
2021-06-03 09:57:55 -0700

    chrome notifications pops up when crypto assets go down

--- background.js
+++ background.js
@@ -1,41 +1,62 @@
-
 let cryptoAssetObject = {}
 let marketPrice = 0
 let cryptoBase = ''
 let currentPrice = 0
 let message = ''
 let title = ''
-
-function calculatePercentage() {
-    const difference = currentPrice - marketPrice
-    const result = Math.floor((difference/marketPrice))
- 
-    if (result < 1) {
-     currentPrice = result*100
-    }
- }
+let startInterval = 0
+let percentage = 0
 
 function assignValues() {
+    currentPrice = marketPrice
     cryptoBase = cryptoAssetObject.data.base
     marketPrice = cryptoAssetObject.data.amount
-    message = `${currentPrice}% decrease. Immediately go to the meditation app.`
+    if (currentPrice) {
+        percentage = (currentPrice - marketPrice) / currentPrice * 100;
+        if (percentage < 0) {
+            percentage = percentage * (-1);
+        }
+        percentage = percentage.toFixed(8);
+    }
+
+    message = `${percentage}% decrease. Immediately go to the meditation app.`
     title = `Uh-oh, ${cryptoBase} dropped.`
 }
 
 function fetchCryptoAsset() {
-    fetch('https://api.coinbase.com/v2/prices/BTC-USD/spot').then(function (response) {
-    return response.json();
+    fetch("https://api.coinbase.com/v2/prices/BTC-USD/spot").then(function (response) {
+        return response.json();
     }).then(function (data) {
         cryptoAssetObject = data
-        calculatePercentage()
-        assignValues()
+        assignValues();
     }).catch(function (err) {
-    console.log("Something went wrong", err)
-    }) 
+        console.log("Something went wrong", err)
+    })
 }
 
-// setInterval(() => {
-//     fetchCryptoAsset()
-//     console.log("message", message)
-//     console.log("title", title)
-// }, 1000)
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+    console.log("message from background script", msg)
+    if ((msg.from === "pop-up") && (msg.subject === "start")) {
+        startInterval = setInterval(() => {
+            fetchCryptoAsset();
+            if ((marketPrice > currentPrice) && currentPrice) {
+                showNotification();
+            }
+        }, 10000)
+    }
+
+    if ((msg.from === "pop-up") && (msg.subject === "stop")) {
+        clearInterval(startInterval)
+    }
+})
+
+function showNotification() {
+    console.log("show notifiction")
+    chrome.notifications.create('reminder', {
+        type: 'basic',
+        iconUrl: 'waking-up.png',
+        title: title,
+        message: message
+    });
+}
+
