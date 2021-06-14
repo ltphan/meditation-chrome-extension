fe787d32a319dd0eb330af376ce30ec82ed307d7
Linh <linh@linhs-mbp.lan>
2021-06-14 10:16:02 -0700

    put data in an array of json file

--- videos.js
+++ videos.js
@@ -1,32 +1,33 @@
-const videos = {
-    0: "https://www.youtube.com/embed/tw7XBKhZJh4",
-    1: "https://www.youtube.com/embed/CN-_zzHpcdM",
-    3: "https://www.youtube.com/embed/pbU_NzZiEAI",
-    4: "https://www.youtube.com/embed/jZXgrhXJw9o",
-    5: "https://www.youtube.com/embed/XoUBqZZgUEI",
-
-}
 const video = document.getElementById("youtubeVideo")
 
-function getLengthOfObject(obj) {
-	return Object.keys(obj).length
+// retrieve json file
+function fetchVideoData() {
+    return fetch('./data.json').then((response) => {
+        return response.json()
+    }).then(data => {
+        return data.data
+    }).catch(e => {
+        console.log("error", e)
+    })
 }
 
 function getRandomInt(val) {
 	return Math.floor(Math.random()*val)
 }
 
-function getVideoURL(obj) {
-	const size = getLengthOfObject(obj)
-	const id = getRandomInt(size)
-    return obj[id]
+async function getVideoURL() {
+    const data = await fetchVideoData()
+    const len = data.length
+    const index = getRandomInt(len)
+    return data[index]
 }
 
-function randomizeVideos(obj) {
-    const videoUrl = getVideoURL(obj)
+async function randomizeVideos() {
+    const videoUrl = await getVideoURL()
+    console.log("vid url",videoUrl)
     video.setAttribute("src", videoUrl)
 }
 
 window.addEventListener('load', (event) => {
-    randomizeVideos(videos)
+    randomizeVideos()
 })
