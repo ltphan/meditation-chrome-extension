fbb8f6a9e981cc47a06f8ada64dbd40deb8d6240
Linh <linh@linhs-mbp.lan>
2021-06-30 13:02:38 -0700

    convert data to different object interface that includes a currentprice property

--- background.js
+++ background.js
@@ -1,3 +1,99 @@
+const file = "./data.json"
+
+async function getJSON(jsonFile) {
+    try {
+        const response = await fetch(jsonFile)
+        const data = await response.json()
+        return data.cryptoAssets
+    } catch (e) {
+        throw new Error("Retrieving JSON failed", e)
+    }
+}
+
+function buildURL(param) {
+    return `https://api.coinbase.com/v2/prices/${param}/spot`
+}
+
+async function getURLS() {
+    try {
+        const params = await getJSON(file)
+        return urls = params.map((val) => {
+            return buildURL(val)
+        })
+    } catch (e) {
+        throw new Error("Error building URL", e)
+    }
+}
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data.data
+    } catch (e) {
+        throw new Error("Coinbase API failed", e)
+    }
+}
+
+async function buildListOfPromises(urls) {
+    try {
+        const promises = []
+        for (let i = 0; i < urls.length; i++) {
+            promises.push(fetchData(urls[i]))
+        }
+        return promises
+    } catch (e) {
+        throw new Error("Something went wrong building list of promises", e)
+    }
+}
+
+async function fetchCalls() {
+    try {
+        const urls = await getURLS()
+        const promises = await buildListOfPromises(urls)
+        const data = await getDataFromListOfPromises(promises)
+        return data 
+    } catch (e) {
+        throw new Error("Failed to retrieve promises from Coinbase API on multiple fetches", e)
+    }
+    
+}
+
+async function getDataFromListOfPromises(promises) {
+    try {
+        const data = await Promise.allSettled(promises)
+        return result = data.map((val) => {
+            return val.value
+        })
+    } catch (e) {
+        throw new Error("Failed to retrieve data from list of promises", e)
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function createObject(data) {
+    return { marketPrice: data.amount, currentPrice: 0, base: data.base }
+}
+
+function convertDataArratToObjects(data) {
+    return data.map((val) => {
+        return createObject(val)
+    })
+}
+
+async function main() {
+    try {
+        const data = await fetchCalls()
+        const objects = convertDataArratToObjects(data)
+        return objects 
+    } catch (e) {
+        throw new Error("Converting data to objects failed", e)
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 let marketPrice = 0
 let currentPrice = 0
 let message = ''
@@ -68,3 +164,4 @@ function showNotification() {
     });
 }
 
+fetchCalls()
